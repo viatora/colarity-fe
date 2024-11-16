912f7bac59184d918b887f9f9439a6d2a3e37983
maxitect <maxime.downe@gmail.com>
2024-11-16 18:12:21 +0000

    chore: load Colourgenerator in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,6 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import "./App.css";
+// src/App.tsx
+import ColourGenerator from "./pages/ColourGenerator";
 
-function App() {
-  return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </>
-  );
+export default function App() {
+  return <ColourGenerator />;
 }
-
-export default App;
